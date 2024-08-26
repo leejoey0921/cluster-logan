235a504536e15cc1c8da6607db82cd65566ce4b3
Rayan  CHIKHI <rayan.chikhi@pasteur.fr>
2024-08-26 22:31:58 +0000

    oops

---
+++ analyses/aug26-card/aug26.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+set -e
+prefix=aug26
+rm -f $prefix.faa
+cat *.faa > tmp
+mv tmp $prefix.faa
+diamond makedb -d $prefix --in $prefix.faa
+diamond makeidx -d $prefix --sensitive -s 1
+aws s3 cp $prefix.dmnd s3://serratus-rayan/beetles/ --acl public-read
+aws s3 cp $prefix.dmnd.seed_idx s3://serratus-rayan/beetles/ --acl public-read
+
+rm -f $prefix.fna
+cat *.fna > tmp
+mv tmp $prefix.fna
+aws s3 cp $prefix.fna s3://serratus-rayan/beetles/ --acl public-read
